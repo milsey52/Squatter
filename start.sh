4105118a46536f08d0e771549eee984550b749da
milsey52 <milsey52@gmail.com>
2026-02-04 22:41:58 +0800

    Re-enable migrations in start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ set -e
 # Activate virtual environment
 . /opt/venv/bin/activate
 
-echo "Skipping migrations (alembic temporarily hidden)..."
+echo "Running database migrations..."
+alembic upgrade head
 
 echo "Starting server..."
 uvicorn main:app --host 0.0.0.0 --port $PORT
